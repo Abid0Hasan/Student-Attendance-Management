65dc5f405c8f9d385fb4feee41b0575fe1cf465d
Abid Ibn Habib <34665203+Abid0Hasan@users.noreply.github.com>
2020-03-29 20:31:36 +0600

    Add files via upload

---
+++ sams/inc/footer.php
@@ -0,0 +1,11 @@
+
+<div class="well">
+	<h3>Website: abidhasan.xyz
+<span class="pull-right">Like Us: www.facebook.com</span>
+	</h3>
+</div>
+
+
+</div>
+</body>
+</html>>
---
+++ sams/inc/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Employee/Student Attendence System PHP</title>
+	<link rel="stylesheet" type="text/css" href="inc/bootstrap.min.css"/>
+	<script type="text/javascript" src="inc/bootstrap.min.js"></script>
+	<script type="text/javascript" src="inc/jquery.min.js"></script>
+</head>
+
+<body>
+  <div class="container">
+
+  		<div class="well text-center">
+  	     <h2>Employee/Student Attendence(Roll Call) System PHP</h2>
+  		</div>
+               
+
+
+
+  	
